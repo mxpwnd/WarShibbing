f20e55f45aacc9ffaa8669d76ea3207cf9dd06d5
BigMo <moritz.thomas.93@googlemail.com>
2014-06-15 19:16:14 +0200

    Cleaned up.
    Git messed up the commit. I hope it's clean, now.

--- Warshibbing/src/de/HSMA/OOT/Warshibbing/BusinessLayer/Field.java
+++ Warshibbing/src/de/HSMA/OOT/Warshibbing/BusinessLayer/Field.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.HSMA.OOT.Warshibbing.BusinessLayer;
 
 /**
@@ -45,47 +44,3 @@ public class Field
             return hitted ? "X" : "O";
     }
 }
-=======
-package de.HSMA.OOT.Warshibbing.BusinessLayer;
-
-/**
- *
- * @author Tobias
- */
-public class Field
-{   
-    Object content;
-    boolean hitted = false, marked = false;
-    
-    public Field()
-    {
-        this.content = null;
-    }
-    
-    public Field(Object content)
-    {
-        this.content = content;
-    }
-    
-    public void setMark()
-    {
-        if(IsHit())
-            hitted = true;
-        else
-            marked = true;
-    }
-    
-    public boolean IsHit()
-    {
-        return (content instanceof Ship);
-    }
-    
-    @Override public String toString()
-    {
-        if(!hitted && !marked)
-            return content == null ? " " : content.toString();
-        else
-            return hitted ? "X" : "O";
-    }
-}
->>>>>>> 77bb27fb5558fd4877b11d4e37b8eafd6625da54
--- Warshibbing/src/de/HSMA/OOT/Warshibbing/BusinessLayer/Weapon.java
+++ Warshibbing/src/de/HSMA/OOT/Warshibbing/BusinessLayer/Weapon.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.HSMA.OOT.Warshibbing.BusinessLayer;
 
 /**
@@ -28,34 +27,3 @@ public abstract class Weapon
         
     }
 }
-=======
-package de.HSMA.OOT.Warshibbing.BusinessLayer;
-
-/**
- *
- * @author Tobias
- */
-public abstract class Weapon
-{
-    public abstract int getHits(Ship ship);
-    
-    public static class Cannon extends Weapon
-    {
-        @Override
-        public int getHits(Ship ship)
-        {
-            return 1;
-        }
-    }
-    
-    public static class NuclearWeapon extends Weapon
-    {
-        @Override
-        public int getHits(Ship ship)
-        {
-            return ship.getLength();
-        }
-        
-    }
-}
->>>>>>> 77bb27fb5558fd4877b11d4e37b8eafd6625da54
